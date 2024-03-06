4b95cb7d1ee205bb0a9079051dea44d88de7976e
Nguyễn Thế Phương Huynh <huynhnguyen2032001@gmail.com>
2024-03-06 22:06:47 +0700

    1st

---
+++ Static/src/HinhVuong.java
@@ -0,0 +1,10 @@
+
+public class HinhVuong {
+	public static int dem=0;
+	public int chieuDai;
+	public HinhVuong(int chieuDai) {
+		this.chieuDai = chieuDai;
+		dem=dem+1;
+	} 
+	
+}
---
+++ Static/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HinhVuong hinhVuong1=new HinhVuong(3);
+		HinhVuong hinhVuong2=new HinhVuong(4);
+		System.out.println(HinhVuong.dem);
+		System.out.println(hinhVuong1.chieuDai);
+	}
+
+}
